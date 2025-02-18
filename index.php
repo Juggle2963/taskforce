d0ce5e8e9239432451a73c904fd5a14385af8bff
Lillens <johanandreasson84@gmail.com>
2025-02-18 14:09:41 +0100

    klart

---
+++ index.php
@@ -0,0 +1,220 @@
+<?php
+           if($_SERVER["REQUEST_METHOD"] === "POST"){
+            if(isset($_POST['delete-index'])){
+
+            // $arr = explode(' ', file_get_contents('data.txt'));
+            $arr = file('data.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); //lagt till FIN - FEL
+
+            $index = $_POST["delete-index"] ?? '';
+            // print_r($POST);
+
+            unset($arr[$index]);
+
+           $arr = array_values($arr);
+
+            $data = implode(PHP_EOL, $arr) . PHP_EOL;
+
+            file_put_contents('data.txt', $data);
+            file_put_contents('d.txt', $index, FILE_APPEND);
+
+
+            header("Location: index.php");
+            exit();
+            }
+        }
+
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+            if(isset($_POST['vad'])){
+
+            $vad = trim($_POST['vad'] ?? '');
+            // print_r($POST);
+
+            if(!empty($vad)){
+                $data = implode(' ', [$vad]) . PHP_EOL;
+                file_exists('data.txt') ? file_put_contents('data.txt', $data, FILE_APPEND) : file_put_contents('data.txt', $data);
+            }
+            
+        }
+        if(file_exists('data.txt')){
+          $eraseEmpty =  file('data.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+          $erased = implode(PHP_EOL, $eraseEmpty) . PHP_EOL;
+          file_put_contents('data.txt', $erased);
+        }
+
+        }
+       
+
+        ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+            background-color: #786a5d;
+        }
+
+        .index-container {
+            display: flex;
+            flex-direction: column;
+            max-width: 1140px;
+            min-width: 350px;
+            margin: 0 auto;
+            align-items: center;
+            justify-content: center;
+            background-color: #786a5d;
+
+        }
+
+
+        .container {
+            display: grid;
+            grid-template-columns: 50% 50%;
+            justify-content: center;
+            border: 1px solid black;
+            border-radius: 8px;
+            width: 75%;
+            padding: 2rem;
+            margin-top: 3vh;
+            background-color: #5d786a;
+            column-gap: 2rem;
+
+        }
+
+
+
+        .tasks {
+            display: grid;
+            place-items: center;
+            /* display: flex; */
+            background-color:rgb(80, 66, 93);
+            min-height: 50vh;
+            border-radius: 5%;
+            /* justify-content: center;
+            align-items: center; */
+        }
+
+        .task-box {
+            display: flex;
+            flex-direction: column;
+            width: 80%;
+            height: 80%;
+            background-color: rgb(219, 215, 222);
+            overflow-y: auto;
+  
+        }
+
+        .form {
+
+            background-color: #6a5d78;
+            min-height: 50vh;
+            border-radius: 8px;
+            padding: 1rem;
+             
+            display: flex;
+        }
+
+        .form-form {
+           border-radius: 8px;
+
+           display: flex;
+           width: 100%;
+           flex-direction: column;
+           justify-content: space-between;
+        }
+        textarea{
+            display: flex;
+            height: 75%;
+            background-color: white;
+        }
+        .info{
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            margin-bottom: 0.2rem;
+        }
+
+
+        input[type="submit"] {
+            width: 100%;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
+        .btn:hover{
+            background-color: #45a049;
+        }
+        .btn{
+            /* width: 19%; */
+            background-color: #4CAF50;
+            color: white;
+            padding: 5px 5px;
+            margin-top: 2px;
+            margin-bottom: 2px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+        .input{
+            width:80%;
+        }
+    </style>
+</head>
+
+<body>
+    <main class="index-container">
+
+        <div class="container">
+            <div class="form">
+                <form class="form-form" action="" method="POST">
+                <label for="name">vad</label>
+                <textarea name="vad" id="vad" rows="5" cols="40" placeholder="<---- TaskForce ---> skriv in uppgift"></textarea>
+
+                    <input type="submit" value="submit">
+                </form>
+            </div>
+            <div class="tasks">
+                <div class="task-box">
+                    <?php
+
+                $arr = array_filter(file('data.txt'), function($line) {
+                    return trim($line) !== '';
+                });
+                  
+                    foreach ($arr as $key => $value){
+                        
+                        echo "<div class='info' > <div>" . $value . "</div class='input'> <form action='' method='POST'>
+                         <input type='hidden' name='delete-index' value='$key'>
+                        <button class='btn' type='submit'>Ta bort</button>
+    
+                    </form>
+                    </div>";
+                        
+                    }
+                    ?>
+                </div>
+            </div>
+
+        </div>
+   
+
+    </main>
+    </body>
+
+</html>
